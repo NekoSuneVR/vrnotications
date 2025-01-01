43936201ac96c47b14c2e9eed2c96fd96fcf30d5
NekoSuneVR <Chisdeal2014@hotmail.com>
2025-01-01 21:49:43 +0000

    Update xsOverlay.js

--- modules/xsOverlay.js
+++ modules/xsOverlay.js
@@ -1,38 +1,69 @@
 // xsOverlay.js
-const dgram = require('dgram');
+const WebSocket = require('ws');
 const { v4: uuidv4 } = require('uuid');
 
 class XSOverlay {
     constructor() {
-        this.port = 42069;
-        this.broadcastIP = '127.0.0.1';
-        this.socket = dgram.createSocket('udp4');
+        this.port = 42070; // Default WebSocket port for XSOverlay
+        this.clientName = 'XSOverlay_NodeJS_Example';
+        this.ws = null;
+    }
+
+    connect() {
+        const url = `ws://localhost:${this.port}/?client=${this.clientName}`;
+
+        this.ws = new WebSocket(url);
+
+        this.ws.on('open', () => {
+            console.log('Connected to XSOverlay WebSocket API');
+        });
+
+        this.ws.on('close', () => {
+            console.log('Disconnected from XSOverlay WebSocket API');
+        });
+
+        this.ws.on('error', (err) => {
+            console.error('WebSocket error:', err);
+        });
+
+        this.ws.on('message', (data) => {
+            console.log('Received message from XSOverlay:', data);
+        });
     }
 
     sendNotification(options) {
-        const defaultMsg = {
-            messageType: 1,
-            index: 0,
+        if (!this.ws || this.ws.readyState !== WebSocket.OPEN) {
+            console.error('WebSocket is not connected. Cannot send notification.');
+            return;
+        }
+
+        const notification = {
+            title: 'Default Title',
+            type: 1, // Default type for notification
+            timeout: 6,
+            content: 'Default Content',
             volume: 0.5,
             audioPath: 'default',
-            timeout: 6,
-            title: 'Default Title',
-            content: 'Default Content',
-            icon: 'default',
             height: 120,
             opacity: 1,
+            icon: 'default',
             useBase64Icon: false,
-            sourceApp: 'XSOverlay_NodeJS_Example'
+            ...options,
         };
 
-        const msg = { ...defaultMsg, ...options };
-        const byteBuffer = Buffer.from(JSON.stringify(msg));
+        const message = {
+            sender: this.clientName,
+            target: 'xsoverlay',
+            command: 'SendNotification',
+            jsonData: JSON.stringify(notification),
+            rawData: null,
+        };
 
-        this.socket.send(byteBuffer, 0, byteBuffer.length, this.port, this.broadcastIP, (err) => {
+        this.ws.send(JSON.stringify(message), (err) => {
             if (err) {
                 console.error('Error sending message:', err);
             } else {
-                console.log('Message sent to XSOverlay');
+                console.log('Notification sent to XSOverlay');
             }
         });
     }
